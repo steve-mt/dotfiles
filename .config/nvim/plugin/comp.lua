4f867481fbb18d5bad37f92072dee87aad3e2a04
Steve Azzopardi <steveazz@outlook.com>
2021-11-06 14:54:57 +0100

    nvim: setup rust-analyzer lsp

--- .config/nvim/plugin/comp.lua
+++ .config/nvim/plugin/comp.lua
@@ -22,6 +22,11 @@ cmp.setup({
   }
 })
 
+local capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
 require('lspconfig')['gopls'].setup {
-    capabilities = require('cmp_nvim_lsp').update_capabilities(vim.lsp.protocol.make_client_capabilities())
+    capabilities = capabilities
+}
+
+require('lspconfig')['rust_analyzer'].setup {
+    capabilities = capabilities
 }
