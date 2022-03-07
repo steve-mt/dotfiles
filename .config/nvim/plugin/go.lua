e064d66b7ee1ec2c1c6523fc973347aa9045919e
Steve Azzopardi <sazzopardi@gitlab.com>
2022-03-07 16:13:39 +0100

    nvim: use default gopls settings

--- .config/nvim/plugin/go.lua
+++ .config/nvim/plugin/go.lua
@@ -1,15 +1,3 @@
 local lspconfig = require "lspconfig"
 
-lspconfig.gopls.setup {
-    cmd = {"gopls", "serve"},
-    settings = {
-        gopls = {
-            analyses = {
-                unusedparams = true,
-                unusedwrite = true,
-                shadow = true,
-            },
-            staticcheck = true,
-        },
-    },
-}
+lspconfig.gopls.setup {}
