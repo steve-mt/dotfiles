2d9f8fc7f6f51f2eec66883471129316a6033147
Steve Azzopardi <steveazz@outlook.com>
2023-02-25 20:02:44 +0100

    fix(nvim): tab size

--- .config/nvim/init.lua
+++ .config/nvim/init.lua
@@ -13,13 +13,14 @@ end
 
 vim.opt.rtp:prepend(lazypath)
 
--- Basinc functionality
+-- Basic functionality
 vim.o.clipboard = 'unnamedplus'
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
-vim.o.siftwidth = 4
+vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.smartindent = true
+vim.o.completeopt = "menuone,noselect"
 
 -- Mappings
 vim.g.mapleader = " "
