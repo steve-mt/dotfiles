f5d5b7e030075350f81542112f7916bae1452c8e
Steve Xuereb <sxuereb@gitlab.com>
2023-07-24 10:02:49 +0200

    feat(fish): rancher desktop path

--- .config/fish/config.fish
+++ .config/fish/config.fish
@@ -109,3 +109,7 @@ abbr -a gsw git switch
 abbr -a gr git restore
 abbr -a bdiff 'git log --pretty=format:"%C(yellow)%H | %ad%Cred%d | %Creset%s%Cblue | [%cn] [%ae]" --abbrev-commit --date=relative'
 abbr -a gcp git cherry-pick
+
+### MANAGED BY RANCHER DESKTOP START (DO NOT EDIT)
+set --export --prepend PATH "/Users/steve/.rd/bin"
+### MANAGED BY RANCHER DESKTOP END (DO NOT EDIT)
