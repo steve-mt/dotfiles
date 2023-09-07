c9ef47ef82e096df43acde557599f5287945cb01
Steve Xuereb <sxuereb@gitlab.com>
2023-09-07 11:11:12 +0200

    feat(repo_clean): change to main branch

--- .config/fish/functions/repo_clean.fish
+++ .config/fish/functions/repo_clean.fish
@@ -1,7 +1,20 @@
 function repo_clean
+    git rev-parse --verify -q main > /dev/null 2>&1
+    if test $status -eq 0
+        echo "main"
+        git switch main
+    end
+
+
+    git rev-parse --verify -q master > /dev/null 2>&1
+    if test $status -eq 0
+        git switch master
+    end
+
+
     echo 'Delelting all branches apart form main'
     git branch | rg -v 'master|main' | parallel --trim rl git branch -D {}
-    echo 'Puring remote'
+    echo 'Pruning remote'
     git pull --prune
     echo 'Running git gc'
     git gc
