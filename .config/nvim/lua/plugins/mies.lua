748223a22ce298a51277ad0cf5e15e6eb3ebaa49
Steve Xuereb <sxuereb@gitlab.com>
2023-12-19 11:15:04 +0100

    fix(nvim): SignColumn color

--- .config/nvim/lua/plugins/mies.lua
+++ .config/nvim/lua/plugins/mies.lua
@@ -5,6 +5,9 @@ return {
 			vim.cmd.colorscheme("mies")
 			vim.o.background = "light"
 			vim.o.termguicolors = true
+
+			-- Set the SignColumn highlight group to white
+			vim.cmd("highlight SignColumn guibg=white ctermbg=white")
 		end,
 	},
 }
