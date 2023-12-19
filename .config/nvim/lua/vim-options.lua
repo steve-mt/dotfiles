88dacd6511fd38b40ebe43c667bdf602461c333d
Steve Xuereb <sxuereb@gitlab.com>
2023-12-19 11:17:48 +0100

    feat(nvim): Copy default vim options from kickstart
    What
    ---
    Copy some of the default vim options defined in https://github.com/nvim-lua/kickstart.nvim/blob/master/init.lua

--- .config/nvim/lua/vim-options.lua
+++ .config/nvim/lua/vim-options.lua
@@ -1,11 +1,36 @@
--- Basic functionality
+-- Sync clipboard between OS and Neovim.
 vim.o.clipboard = "unnamedplus"
+
 vim.o.tabstop = 4
 vim.o.softtabstop = 4
 vim.o.shiftwidth = 4
 vim.o.expandtab = true
 vim.o.smartindent = true
+
+-- Set completeopt to have a better completion experience
 vim.o.completeopt = "menuone,noselect"
+
+-- Case-insensitive searching UNLESS \C or capital in search
+vim.o.ignorecase = true
+vim.o.smartcase = true
+
+-- Keep signcolumn on by default
+vim.wo.signcolumn = "yes"
+
+-- [[ Highlight on yank ]]
+-- See `:help vim.highlight.on_yank()`
+local highlight_group = vim.api.nvim_create_augroup("YankHighlight", { clear = true })
+vim.api.nvim_create_autocmd("TextYankPost", {
+	callback = function()
+		vim.highlight.on_yank()
+	end,
+	group = highlight_group,
+	pattern = "*",
+})
+
+-- Decrease update time
+vim.o.updatetime = 250
+vim.o.timeoutlen = 300
 
 -- Mappings
 vim.g.mapleader = " "
