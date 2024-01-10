c5a8d78d970325a5e4addbefeaeecd49bd594022
Steve Xuereb <sxuereb@gitlab.com>
2024-01-10 09:35:59 +0100

    refactor(nvim): change liniting and formatting
    What
    ---
    * Remove `conform`
    * Remove `lint`
    * Install `none-ls` for formatting and linting

---
+++ .config/nvim/lua/plugins/none_ls.lua
@@ -0,0 +1,54 @@
+return {
+	{
+		"nvimtools/none-ls.nvim",
+		event = {
+			"BufReadPre",
+			"BufNewFile",
+		},
+		config = function()
+			local augroup = vim.api.nvim_create_augroup("LspFormatting", {})
+			local null_ls = require("null-ls")
+
+			null_ls.setup({
+				sources = {
+					-- Formatters
+					null_ls.builtins.formatting.stylua,
+					null_ls.builtins.formatting.trim_whitespace,
+					null_ls.builtins.formatting.trim_newlines,
+					null_ls.builtins.formatting.rustfmt.with({
+						extra_args = { "--edition=2021" },
+					}),
+					null_ls.builtins.formatting.jq,
+					null_ls.builtins.formatting.gofumpt,
+					null_ls.builtins.formatting.rubocop,
+					null_ls.builtins.formatting.yamlfix,
+					null_ls.builtins.formatting.terraform_fmt,
+					null_ls.builtins.formatting.prettier,
+					null_ls.builtins.formatting.markdownlint,
+					null_ls.builtins.formatting.jsonnetfmt,
+
+					-- Linters
+					null_ls.builtins.diagnostics.trivy,
+					null_ls.builtins.diagnostics.terraform_validate,
+					null_ls.builtins.diagnostics.shellcheck,
+					null_ls.builtins.diagnostics.yamllint,
+					null_ls.builtins.diagnostics.markdownlint.with({
+						extra_args = { "--disable", "MD013", "MD033" },
+					}),
+				},
+				on_attach = function(client, bufnr) -- fmt on save https://github.com/nvimtools/none-ls.nvim/wiki/Formatting-on-save
+					if client.supports_method("textDocument/formatting") then
+						vim.api.nvim_clear_autocmds({ group = augroup, buffer = bufnr })
+						vim.api.nvim_create_autocmd("BufWritePre", {
+							group = augroup,
+							buffer = bufnr,
+							callback = function()
+								vim.lsp.buf.format({ async = false })
+							end,
+						})
+					end
+				end,
+			})
+		end,
+	},
+}
