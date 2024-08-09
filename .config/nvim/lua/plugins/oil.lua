b793895fc136a335f6d0d2962b85cd306f974539
Steve Xuereb <sxuereb@gitlab.com>
2024-08-09 09:27:50 +0200

    feat(nvim): disable oil keybindings

--- .config/nvim/lua/plugins/oil.lua
+++ .config/nvim/lua/plugins/oil.lua
@@ -3,6 +3,14 @@ return {
 		"stevearc/oil.nvim",
 		config = function()
 			require("oil").setup({
+				columns = { "icon" },
+				keymaps = {
+					["<C-h>"] = false,
+					["<C-l>"] = false,
+					["<C-k>"] = false,
+					["<C-j>"] = false,
+					["<M-h>"] = "actions.select_split",
+				},
 				view_options = {
 					show_hidden = true,
 				},
