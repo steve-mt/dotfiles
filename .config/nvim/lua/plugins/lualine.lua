4ef2c2f00c5d89b93d639ba649730c9541b7c648
Steve Xuereb <sxuereb@gitlab.com>
2024-08-12 13:05:06 +0200

    refactor(nvim): update lualine colors

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -2,20 +2,33 @@ return {
 	{
 		"hoob3rt/lualine.nvim",
 		config = function()
+			local theme = require("lualine.themes.onelight")
+			theme.normal.a.bg = "#d0d0d0"
+			theme.normal.a.fg = "#494b53"
+			theme.command.a.bg = "#d0d0d0"
+			theme.command.a.fg = "#494b53"
+			theme.visual.a.bg = "#d0d0d0"
+			theme.visual.a.fg = "#494b53"
+			theme.replace.a.bg = "#d0d0d0"
+			theme.replace.a.fg = "#494b53"
+			theme.insert.a.bg = "#d0d0d0"
+			theme.insert.a.fg = "#494b53"
 			require("lualine").setup({
 				options = {
 					icons_enabled = false,
-					theme = "onelight",
+					theme = theme,
 					component_separators = { "", "" },
 					section_separators = { "", "" },
 					disabled_filetypes = {},
 				},
 				sections = {
 					lualine_a = { "mode" },
-					lualine_b = { {
-						"filename",
-						path = 1,
-					} },
+					lualine_b = {
+						{
+							"filename",
+							path = 1,
+						},
+					},
 					lualine_c = {},
 					lualine_x = { "filetype" },
 					lualine_y = {},
@@ -23,10 +36,12 @@ return {
 				},
 				inactive_sections = {
 					lualine_a = {},
-					lualine_b = { {
-						"filename",
-						path = 1,
-					} },
+					lualine_b = {
+						{
+							"filename",
+							path = 1,
+						},
+					},
 					lualine_c = {},
 					lualine_x = {},
 					lualine_y = {},
