c132ee01fa04eb3a2f495d957f8d54ca4aa4ee52
Steve Xuereb <sxuereb@gitlab.com>
2024-08-13 10:16:15 +0200

    refactor(vim): lualine theme

--- .config/nvim/lua/plugins/lualine.lua
+++ .config/nvim/lua/plugins/lualine.lua
@@ -3,16 +3,18 @@ return {
 		"hoob3rt/lualine.nvim",
 		config = function()
 			local theme = require("lualine.themes.onelight")
-			theme.normal.a.bg = "#d0d0d0"
+			theme.normal.a.bg = "#f0f0f0"
 			theme.normal.a.fg = "#494b53"
-			theme.command.a.bg = "#d0d0d0"
+			theme.normal.b.bg = "#f0f0f0"
+			theme.command.a.bg = "#f0f0f0"
 			theme.command.a.fg = "#494b53"
-			theme.visual.a.bg = "#d0d0d0"
+			theme.visual.a.bg = "#f0f0f0"
 			theme.visual.a.fg = "#494b53"
-			theme.replace.a.bg = "#d0d0d0"
+			theme.replace.a.bg = "#f0f0f0"
 			theme.replace.a.fg = "#494b53"
-			theme.insert.a.bg = "#d0d0d0"
+			theme.insert.a.bg = "#f0f0f0"
 			theme.insert.a.fg = "#494b53"
+
 			require("lualine").setup({
 				options = {
 					icons_enabled = false,
