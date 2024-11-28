f14ee376f1426831941ea5ca28a915453aa615bd
Steve Xuereb <sxuereb@gitlab.com>
2024-11-28 08:18:52 +0100

    fix(nvim): disable protolint
    When saving `*.proto` fails it's causing an error

--- .config/nvim/lua/plugins/mason_tool_installer.lua
+++ .config/nvim/lua/plugins/mason_tool_installer.lua
@@ -34,7 +34,6 @@ return {
 					"vale",
 					"trivy",
 					"codespell",
-					"protolint",
 				},
 			})
 		end,
--- .config/nvim/lua/plugins/none_ls.lua
+++ .config/nvim/lua/plugins/none_ls.lua
@@ -35,7 +35,6 @@ return {
 					}),
 					null_ls.builtins.diagnostics.codespell,
 					null_ls.builtins.diagnostics.vale,
-					null_ls.builtins.formatting.protolint,
 				},
 				on_attach = function(client, bufnr) -- fmt on save https://github.com/nvimtools/none-ls.nvim/wiki/Formatting-on-save
 					if client.supports_method("textDocument/formatting") then
