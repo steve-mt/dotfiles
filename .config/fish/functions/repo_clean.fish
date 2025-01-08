cbf4d11cb924fd3e37ef6dd1d3c76f9eff3ee260
Steve Xuereb <sxuereb@gitlab.com>
2025-01-08 08:02:57 +0100

    refactor(repo_clean): show messages with emoji

--- .config/fish/functions/repo_clean.fish
+++ .config/fish/functions/repo_clean.fish
@@ -1,21 +1,21 @@
 function repo_clean
+    echo "📣 Reset to default branch"
+    git reset --hard @
     git rev-parse --verify -q main > /dev/null 2>&1
     if test $status -eq 0
-        echo "main"
         git switch main
     end
-
-
     git rev-parse --verify -q master > /dev/null 2>&1
     if test $status -eq 0
         git switch master
     end
 
-
-    echo 'Delelting all branches apart form main'
+    echo '📣 Delelting all branches apart form main'
     git branch | rg -v 'master|main' | parallel --trim rl git branch -D {}
-    echo 'Pruning remote'
+
+    echo '📣 Pruning remote'
     git pull --prune
-    echo 'Running git gc'
+
+    echo '📣 Running git gc'
     git gc
 end
