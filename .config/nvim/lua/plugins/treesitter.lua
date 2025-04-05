7d7a01450131454a48f3d40f60fc118b219c1f63
Steve Xuereb <sxuereb@gitlab.com>
2025-04-05 08:48:53 +0200

    refactor(nvim): treesitter config
    What
    ---
    1. Remove conflicting keybinds
    2. Remove extra indentation
    
    Why
    ---
    1. Right now I don't use the incremental_selection

--- .config/nvim/lua/plugins/treesitter.lua
+++ .config/nvim/lua/plugins/treesitter.lua
@@ -1,51 +1,38 @@
 return {
 	{
+		"nvim-treesitter/nvim-treesitter",
+		build = ":TSUpdate",
+		config = function()
+			require("nvim-treesitter.configs").setup({
+				ensure_installed = {
+					"bash",
+					"rust",
+					"go",
+					"gomod",
+					"gowork",
+					"make",
+					"yaml",
+					"dockerfile",
+					"hcl",
+					"json",
+					"lua",
+					"toml",
+					"ruby",
+					"vim",
+					"jsonnet",
+					"javascript",
+					"terraform",
+				},
 
-		{
-			"nvim-treesitter/nvim-treesitter",
-			build = ":TSUpdate",
-			config = function()
-				require("nvim-treesitter.configs").setup({
-					ensure_installed = {
-						"bash",
-						"rust",
-						"go",
-						"gomod",
-						"gowork",
-						"make",
-						"yaml",
-						"dockerfile",
-						"hcl",
-						"json",
-						"lua",
-						"toml",
-						"ruby",
-						"vim",
-						"jsonnet",
-						"javascript",
-						"terraform",
-					},
+				highlight = {
+					enable = true,
+					custom_captures = {},
+				},
 
-					highlight = {
-						enable = true,
-						custom_captures = {},
-					},
-
-					incremental_selection = {
-						enable = true,
-						keymaps = {
-							init_selection = "gnn",
-							node_incremental = "grn",
-							scope_incremental = "grc",
-							node_decremental = "grm",
-						},
-					},
-
-					indent = {
-						enable = true,
-					},
-				})
-			end,
-		},
+				indent = {
+					enable = true,
+				},
+			})
+		end,
 	},
 }
