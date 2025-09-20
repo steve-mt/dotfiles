d9396ff1ba8c377ee1bd429c5804e57b158abc08
Steve <steveazz@outlook.com>
2025-09-20 07:16:34 +0200

    fix(nvim): lspconfig deprecation warning
    Started getting:
    ```shell
    The `require('lspconfig')` "framework" is deprecated, use vim.lsp.config (see :help lspconfig-nvim-0.11) instead.
    
    Feature will be removed in nvim-lspconfig v3.0.
    ```
    
    I copied the code from [kickstart](https://github.com/nvim-lua/kickstart.nvim/blob/3338d3920620861f8313a2745fd5d2be39f39534/init.lua#L722C7-L735C8)

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -69,11 +69,19 @@ return {
 		local capabilities = vim.lsp.protocol.make_client_capabilities()
 		capabilities = require("blink.cmp").get_lsp_capabilities(capabilities)
 
-		for server_name, settings in pairs(servers) do
-			require("lspconfig")[server_name].setup({
-				capabilities = capabilities,
-				settings = settings,
-			})
-		end
+		require("mason-lspconfig").setup({
+			ensure_installed = {}, -- explicitly set to an empty table (Kickstart populates installs via mason-tool-installer)
+			automatic_installation = false,
+			handlers = {
+				function(server_name)
+					local server = servers[server_name] or {}
+					-- This handles overriding only values explicitly passed
+					-- by the server configuration above. Useful when disabling
+					-- certain features of an LSP (for example, turning off formatting for ts_ls)
+					server.capabilities = vim.tbl_deep_extend("force", {}, capabilities, server.capabilities or {})
+					require("lspconfig")[server_name].setup(server)
+				end,
+			},
+		})
 	end,
 }
