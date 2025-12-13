a0cf0a8d42568729e67c7624065eeb1aca0b612d
Steve Xuereb <hi@steve.mt>
2025-12-13 06:50:31 +0100

    refactor(nvim): change colorscheme to mies again
    What
    ---
    Revert back to 4e411e1ea8221e08fa05cef5eb0afc797598177f colorscheme

--- .config/nvim/lua/plugins/colorscheme.lua
+++ .config/nvim/lua/plugins/colorscheme.lua
@@ -1,32 +1,11 @@
 return {
-	"jackplus-xyz/binary.nvim",
-	priority = 1000,
-	opts = {
-		style = "light",
-		colors = {
-			fg = "#080808",
-			bg = "#ffffff",
-		},
-		-- These groups will have reversed colors (bg becomes fg, fg becomes bg)
-		-- for better visibility on important UI elements
-		reversed_group = {
-			-- Editor UI
-			Cursor = true,
-			IncSearch = true,
-			MatchParen = true,
-			PmenuSel = true,
-			Search = true,
-			Visual = true,
-			-- LSP
-			LspReferenceText = true,
-			LspReferenceRead = true,
-			LspReferenceWrite = true,
-			-- Telescope
-			TelescopeSelection = true,
-		},
-	},
-	config = function(_, opts)
-		require("binary").setup(opts)
-		vim.cmd("colorscheme binary")
+	"jaredgorski/Mies.vim",
+	config = function()
+		vim.cmd.colorscheme("mies")
+		vim.o.background = "light"
+		vim.o.termguicolors = true
+
+		-- Set the SignColumn highlight group to white
+		vim.cmd("highlight SignColumn guibg=white ctermbg=white")
 	end,
 }
