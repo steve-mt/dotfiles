57f23e79b726d433f862d209f5ca97537c0b9935
Steve Xuereb <hi@steve.mt>
2025-12-14 06:35:13 +0100

    nvim(refactor): luastyle blame.nvim config

--- .config/nvim/lua/plugins/blame.lua
+++ .config/nvim/lua/plugins/blame.lua
@@ -1,10 +1,9 @@
 return {
-    "FabijanZulj/blame.nvim",
-    lazy = false,
-    config = function()
-        require("blame").setup({})
-    end,
-    keys = {
-        { "<leader>gb", "<cmd>BlameToggle<cr>", desc = "Toggle git blame" },
-    },
+	"FabijanZulj/blame.nvim",
+	config = function()
+		require("blame").setup({})
+	end,
+	keys = {
+		{ "<leader>gb", "<cmd>BlameToggle<cr>", desc = "Toggle git blame" },
+	},
 }
