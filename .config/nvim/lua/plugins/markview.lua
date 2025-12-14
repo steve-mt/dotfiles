d284b42d9bec7b8025cad63f66de0546fa5b75ca
Steve Xuereb <hi@steve.mt>
2025-12-14 06:40:59 +0100

    refactor(nvim): markview lazyloading

--- .config/nvim/lua/plugins/markview.lua
+++ .config/nvim/lua/plugins/markview.lua
@@ -1,9 +1,9 @@
 return {
-    "OXY2DEV/markview.nvim",
-    lazy = false,
-    config = function()
-        require("markview").setup()
+	"OXY2DEV/markview.nvim",
+	ft = "markdown",
+	config = function()
+		require("markview").setup()
 
-        vim.keymap.set("n", "<leader>mv", "<cmd>Markview toggle<cr>", { desc = "Toggle Markview" })
-    end,
+		vim.keymap.set("n", "<leader>mv", "<cmd>Markview toggle<cr>", { desc = "Toggle Markview" })
+	end,
 }
