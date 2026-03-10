872d49ed1277528b250448280a15789b7717899a
Steve Xuereb <hi@steve.mt>
2026-03-10 06:31:52 +0100

    fix(nvim): lua lsp
    What
    ---
    - Enable luajit
    - Add `vim` to globals

--- .config/nvim/lua/plugins/lsp.lua
+++ .config/nvim/lua/plugins/lsp.lua
@@ -74,7 +74,12 @@ return {
 			lua_ls = {
 				settings = {
 					Lua = {
-						workspace = { checkThirdParty = false },
+						runtime = { version = "LuaJIT" },
+						diagnostics = { globals = { "vim" } },
+						workspace = {
+							checkThirdParty = false,
+							library = vim.api.nvim_get_runtime_file("", true),
+						},
 						telemetry = { enable = false },
 					},
 				},
